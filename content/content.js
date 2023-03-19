d4db2a04e52f5848aa1d704f9e24be71fb47d2ec
Harsh Shah <harshshah1295@gmail.com>
2023-03-19 15:05:54 -0400

    - Upgraded the extension to V3 - Fixed minor issues

--- content/content.js
+++ content/content.js
@@ -1,10 +1,8 @@
 (function () {
-
-    let abbreviations = {}
-    let services = {}
+    let abbreviations = {};
+    let services = {};
 
     function initialize() {
-
         loadServices();
 
         loadAbbreviations();
@@ -17,12 +15,11 @@
     }
 
     function loadObservers() {
-
         const targetNode = document.body;
 
         const config = { childList: true, subtree: true };
 
-        const observer = new MutationObserver(function (mutationsList, observer) {
+        const observer = new MutationObserver(function () {
             scanForEditableFields();
         });
 
@@ -30,83 +27,67 @@
     }
 
     function scanForEditableFields() {
-
         let documents = [document];
-        let editableElements = []
+        let editableElements = [];
 
-        let iframes = document.querySelectorAll('iframe');
+        let iframes = document.querySelectorAll("iframe");
 
         for (let iframe of iframes) {
             if (iframe.contentDocument) {
-                documents.push(iframe.contentDocument)
+                documents.push(iframe.contentDocument);
             }
         }
 
         for (let doc of documents) {
-            editableElements.push(...doc.querySelectorAll("textarea"))
-            editableElements.push(...doc.querySelectorAll("[contenteditable=true]"))
-            editableElements.push(...doc.querySelectorAll("[type=text]"))
-            editableElements.push(...doc.querySelectorAll("[type=email]"))
+            editableElements.push(...doc.querySelectorAll("textarea"));
+            editableElements.push(
+                ...doc.querySelectorAll("[contenteditable=true]")
+            );
+            editableElements.push(...doc.querySelectorAll("[type=text]"));
+            editableElements.push(...doc.querySelectorAll("[type=email]"));
         }
 
         for (let editableElement of editableElements) {
             if (new Set(["INPUT", "TEXTAREA"]).has(editableElement.nodeName)) {
-
-                editableElement.addEventListener("keyup", listenerForAbbreviations);
-                editableElement.addEventListener("keyup", listenerForLoremipsum);
-
+                editableElement.removeEventListener("keyup", listener);
+                editableElement.addEventListener("keyup", listener);
             } else if (new Set(["DIV", "P"]).has(editableElement.nodeName)) {
-
                 observerForAbbreviations(editableElement);
-
             }
         }
     }
 
-    function listenerForLoremipsum() {
-        if (!services.loremipsum) {
-            return
-        }
-
-        if (!e.target.value) {
-            return;
-        }
-
-        let pharses = e.target.value.match(/=lorem\([0-9]*\)/ig)
-        if (pharses) {
-            pharses = pharses.map(pharse => pharse.substr(1))
-            checkForLoremipsum(pharses, e.target)
-        }
+    function listener(e) {
+        e.target.value = replaceAbbreviations(e.target.value);
     }
 
-    function listenerForAbbreviations(e) {
+    function replaceAbbreviations(value) {
         if (!services.abbreviator) {
-            return
+            return value;
         }
 
-        if (!e.target.value) {
-            return;
+        if (!value) {
+            return value;
         }
 
-        let pharses = e.target.value.match(/=[a-zA-Z]+/ig)
+        let pharses = value.match(/=[a-zA-Z0-9]+/gi);
         if (pharses) {
-            pharses = pharses.map(pharse => pharse.substr(1))
-
-            let response = checkForAbbreviations(pharses, e.target.value)
-
+            pharses = pharses.map((pharse) => pharse.substr(1));
+            let response = checkForAbbreviations(pharses, value);
             if (response.replacement) {
-                e.target.value = response.content;
+                return response.content;
             }
         }
+
+        return value;
     }
 
     function observerForAbbreviations(editableElement) {
-
         let config = { childList: true, subtree: true, characterData: true };
 
         let observer = new MutationObserver(function (mutations, observer) {
             if (!services.abbreviator) {
-                return
+                return;
             }
 
             let content = mutations[0].target.textContent;
@@ -115,11 +96,11 @@
                 return;
             }
 
-            let pharses = content.match(/=[a-zA-Z]+/ig)
+            let pharses = content.match(/=[a-zA-Z]+/gi);
             if (pharses) {
-                pharses = pharses.map(pharse => pharse.substr(1))
+                pharses = pharses.map((pharse) => pharse.substr(1));
 
-                let response = checkForAbbreviations(pharses, content)
+                let response = checkForAbbreviations(pharses, content);
 
                 if (response.replacement) {
                     mutations[0].target.textContent = response.content;
@@ -131,7 +112,11 @@
     }
 
     function loadListners() {
-        chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        chrome.runtime.onMessage.addListener(function (
+            request,
+            sender,
+            sendResponse
+        ) {
             if (request.todo == "CHANGE_IN_ABBREVIATIONS") {
                 loadAbbreviations();
             } else if (request.todo == "SERVICE_STATUS_UPDATE") {
@@ -143,34 +128,31 @@
     function loadAbbreviations() {
         chrome.storage.sync.get("abbreviations", function (response) {
             abbreviations = response.abbreviations;
-        })
+        });
     }
 
     function loadServices() {
         chrome.storage.sync.get("services", function (response) {
             services = response.services ? response.services : {};
-        })
+        });
     }
 
     function checkForAbbreviations(pharses, content) {
-
         let originalContent = content;
         let replacement = false;
 
         for (let pharse of pharses) {
             if (abbreviations[pharse]) {
                 replacement = true;
-                originalContent = originalContent.replaceAll("=" + pharse, abbreviations[pharse]);
+                originalContent = originalContent.replaceAll(
+                    "=" + pharse,
+                    abbreviations[pharse]
+                );
             }
         }
 
-        return { "content": originalContent, "replacement": replacement }
-    }
-
-    function checkForLoremipsum(pharses, target) {
-
+        return { content: originalContent, replacement: replacement };
     }
 
     initialize();
-
 })();
