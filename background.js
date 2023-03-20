2ce9a194fd6943de945058ca358e9cc83d8b8350
Harsh Shah <harshshah1295@gmail.com>
2023-03-19 21:52:52 -0400

    - Upgraded the extension to V3 - Fixed minor issues

--- background.js
+++ background.js
@@ -1,60 +1,73 @@
-(function(){
-
-    function enableContextMenus(){
+(function () {
+    function enableContextMenus() {
         let menuItem = {
-            "id": "utilityX",
-            "title": "Add Abbreviations",
-            "contexts": ["selection"]
+            id: "utilityX",
+            title: "Add Abbreviations",
+            contexts: ["selection"],
         };
         chrome.contextMenus.create(menuItem);
-    
-        chrome.contextMenus.onClicked.addListener(function(clickData){   
-            if (clickData.menuItemId == "utilityX" && clickData.selectionText){  
-                    let abbreviation = prompt("Abbreviation: ");         
-                    chrome.storage.sync.get(['abbreviations'], function(response){
 
-                        let abbreviations = response["abbreviations"]
-                        abbreviations = abbreviations ? abbreviations : {};
-                        let fullform = clickData.selectionText
-                        
-                        if(!abbreviations[abbreviation]){
-                            abbreviations[abbreviation] = fullform
-            
-                            chrome.storage.sync.set({"abbreviations" : abbreviations}, function(){
-            
-                                sendMessageToAllTabs({todo: "CHANGE_IN_ABBREVIATIONS"})
+        chrome.contextMenus.onClicked.addListener(function (clickData) {
+            if (clickData.menuItemId == "utilityX" && clickData.selectionText) {
 
-                                var notifOptions = {
-                                    type: "basic",
-                                    title: "Added!",
-                                    iconUrl: "assets/16px.png",
-                                    message: "Added! Abbreviation addded successfully"
-                                };
-                                chrome.notifications.create('ABBREVIATION_ADDED', notifOptions);
-                            })
-                        } else {
+                // TODO: Take user input here
+                let abbreviation = "Sample";
+                chrome.storage.sync.get(["abbreviations"], function (response) {
+                    let abbreviations = response["abbreviations"];
+                    abbreviations = abbreviations ? abbreviations : {};
+                    let fullform = clickData.selectionText;
 
-                            var notifOptions = {
+                    if (!abbreviations[abbreviation]) {
+                        abbreviations[abbreviation] = fullform;
+
+                        chrome.storage.sync.set(
+                            { abbreviations: abbreviations },
+                            function () {
+                                sendMessageToAllTabs({
+                                    todo: "CHANGE_IN_ABBREVIATIONS",
+                                });
+
+                                chrome.notifications.create(
+                                    "ABBREVIATION_ADDED",
+                                    {
+                                        type: "basic",
+                                        title: "Added!",
+                                        iconUrl: "assets/16px.png",
+                                        message:
+                                            "Added! Abbreviation addded successfully",
+                                        priority: 1,
+                                    }
+                                );
+                            }
+                        );
+                    } else {
+                        chrome.notifications.create(
+                            "ABBREVIATION_ADDITION_FAILED",
+                            {
                                 type: "basic",
                                 title: "Error",
                                 iconUrl: "assets/16px.png",
-                                message: "Uh oh, Already have one abbreviation with this key"
-                            };
-                            chrome.notifications.create('ABBREVIATION_ADDITION_FAILED', notifOptions);
-                        }
-                    });
+                                message:
+                                    "Uh oh, Already have one abbreviation with this key",
+                                priority: 1,
+                            }
+                        );
+                    }
+                });
             }
         });
     }
 
-    function sendMessageToAllTabs(message){
-        chrome.tabs.query({}, function(tabs){
-            for(let tab of tabs){
-                chrome.tabs.sendMessage(tab.id, message);
-            }
-        })
+    function sendMessageToAllTabs(message) {
+        (async () => {
+            const [tab] = await chrome.tabs.query({
+                active: true,
+                lastFocusedWindow: true,
+            });
+            const response = await chrome.tabs.sendMessage(tab.id, message);
+            console.log(response);
+        })();
     }
 
     enableContextMenus();
-
 })();
--- content/content.js
+++ content/content.js
@@ -1,158 +1,147 @@
+let abbreviations = {};
+let services = {};
+
 (function () {
-    let abbreviations = {};
-    let services = {};
+    loadServices();
 
-    function initialize() {
-        loadServices();
+    loadAbbreviations();
 
-        loadAbbreviations();
+    addRuntimeListners();
 
-        loadListners();
+    scanForEditableFields();
 
+    loadObservers();
+})();
+
+function loadObservers() {
+    const targetNode = document.body;
+
+    const config = { childList: true, subtree: true };
+
+    const observer = new MutationObserver(function () {
         scanForEditableFields();
+    });
 
-        loadObservers();
-    }
+    observer.observe(targetNode, config);
+}
 
-    function loadObservers() {
-        const targetNode = document.body;
+function scanForEditableFields() {
+    let documents = [document];
+    let editableElements = [];
 
-        const config = { childList: true, subtree: true };
+    let iframes = document.querySelectorAll("iframe");
 
-        const observer = new MutationObserver(function () {
-            scanForEditableFields();
-        });
-
-        observer.observe(targetNode, config);
-    }
-
-    function scanForEditableFields() {
-        let documents = [document];
-        let editableElements = [];
-
-        let iframes = document.querySelectorAll("iframe");
-
-        for (let iframe of iframes) {
-            if (iframe.contentDocument) {
-                documents.push(iframe.contentDocument);
-            }
-        }
-
-        for (let doc of documents) {
-            editableElements.push(...doc.querySelectorAll("textarea"));
-            editableElements.push(
-                ...doc.querySelectorAll("[contenteditable=true]")
-            );
-            editableElements.push(...doc.querySelectorAll("[type=text]"));
-            editableElements.push(...doc.querySelectorAll("[type=email]"));
-        }
-
-        for (let editableElement of editableElements) {
-            if (new Set(["INPUT", "TEXTAREA"]).has(editableElement.nodeName)) {
-                editableElement.removeEventListener("keyup", listener);
-                editableElement.addEventListener("keyup", listener);
-            } else if (new Set(["DIV", "P"]).has(editableElement.nodeName)) {
-                observerForAbbreviations(editableElement);
-            }
+    for (let iframe of iframes) {
+        if (iframe.contentDocument) {
+            documents.push(iframe.contentDocument);
         }
     }
 
-    function listener(e) {
-        e.target.value = replaceAbbreviations(e.target.value);
+    for (let doc of documents) {
+        editableElements.push(...doc.querySelectorAll("textarea"));
+        editableElements.push(
+            ...doc.querySelectorAll("[contenteditable=true]")
+        );
+        editableElements.push(...doc.querySelectorAll("[type=text]"));
+        editableElements.push(...doc.querySelectorAll("[type=email]"));
     }
 
-    function replaceAbbreviations(value) {
-        if (!services.abbreviator) {
-            return value;
+    for (let editableElement of editableElements) {
+        if (new Set(["INPUT", "TEXTAREA"]).has(editableElement.nodeName)) {
+            editableElement.removeEventListener("keyup", listener);
+            editableElement.addEventListener("keyup", listener);
+        } else if (new Set(["DIV", "P"]).has(editableElement.nodeName)) {
+            observerForAbbreviations(editableElement);
         }
+    }
+}
 
-        if (!value) {
-            return value;
-        }
+function listener(e) {
+    e.target.value = replaceAbbreviations(e.target.value);
+}
 
-        let pharses = value.match(/=[a-zA-Z0-9]+/gi);
-        if (pharses) {
-            pharses = pharses.map((pharse) => pharse.substr(1));
-            let response = checkForAbbreviations(pharses, value);
-            if (response.replacement) {
-                return response.content;
-            }
-        }
-
+function replaceAbbreviations(value) {
+    if (!services.abbreviator) {
         return value;
     }
 
-    function observerForAbbreviations(editableElement) {
-        let config = { childList: true, subtree: true, characterData: true };
-
-        let observer = new MutationObserver(function (mutations, observer) {
-            if (!services.abbreviator) {
-                return;
-            }
-
-            let content = mutations[0].target.textContent;
-
-            if (!content) {
-                return;
-            }
-
-            let pharses = content.match(/=[a-zA-Z]+/gi);
-            if (pharses) {
-                pharses = pharses.map((pharse) => pharse.substr(1));
-
-                let response = checkForAbbreviations(pharses, content);
-
-                if (response.replacement) {
-                    mutations[0].target.textContent = response.content;
-                }
-            }
-        });
-
-        observer.observe(editableElement, config);
+    if (!value) {
+        return value;
     }
 
-    function loadListners() {
-        chrome.runtime.onMessage.addListener(function (
-            request,
-            sender,
-            sendResponse
-        ) {
-            if (request.todo == "CHANGE_IN_ABBREVIATIONS") {
-                loadAbbreviations();
-            } else if (request.todo == "SERVICE_STATUS_UPDATE") {
-                loadServices();
-            }
-        });
+    let pharses = value.match(/=[a-zA-Z0-9]+/gi);
+    if (pharses) {
+        pharses = pharses.map((pharse) => pharse.substr(1));
+        let response = checkForAbbreviations(pharses, value);
+        if (response.replacement) {
+            return response.content;
+        }
     }
 
-    function loadAbbreviations() {
-        chrome.storage.sync.get("abbreviations", function (response) {
-            abbreviations = response.abbreviations;
-        });
-    }
+    return value;
+}
 
-    function loadServices() {
-        chrome.storage.sync.get("services", function (response) {
-            services = response.services ? response.services : {};
-        });
-    }
+function observerForAbbreviations(editableElement) {
+    let config = { childList: true, subtree: true, characterData: true };
 
-    function checkForAbbreviations(pharses, content) {
-        let originalContent = content;
-        let replacement = false;
-
-        for (let pharse of pharses) {
-            if (abbreviations[pharse]) {
-                replacement = true;
-                originalContent = originalContent.replaceAll(
-                    "=" + pharse,
-                    abbreviations[pharse]
-                );
-            }
+    let observer = new MutationObserver(function (mutations, observer) {
+        if (!services.abbreviator) {
+            return;
         }
 
-        return { content: originalContent, replacement: replacement };
+        let content = mutations[0].target.textContent;
+
+        if (!content) {
+            return;
+        }
+
+        let pharses = content.match(/=[a-zA-Z]+/gi);
+        if (pharses) {
+            pharses = pharses.map((pharse) => pharse.substr(1));
+
+            let response = checkForAbbreviations(pharses, content);
+
+            if (response.replacement) {
+                mutations[0].target.textContent = response.content;
+            }
+        }
+    });
+
+    observer.observe(editableElement, config);
+}
+
+function addRuntimeListners() {
+    chrome.runtime.onMessage.addListener(function (request) {
+        if (request.todo == "CHANGE_IN_ABBREVIATIONS") {
+            loadAbbreviations();
+        } else if (request.todo == "SERVICE_STATUS_UPDATE") {
+            loadServices();
+        }
+    });
+}
+
+function loadAbbreviations() {
+    chrome.storage.sync.get("abbreviations", function (response) {
+        abbreviations = response.abbreviations;
+    });
+}
+
+function loadServices() {
+    chrome.storage.sync.get("services", function (response) {
+        services = response.services ? response.services : {};
+    });
+}
+
+function checkForAbbreviations(pharses, content) {
+    let originalContent = content;
+    let replacement = false;
+
+    for (let pharse of pharses) {
+        if (abbreviations[pharse]) {
+            replacement = true;
+            originalContent = originalContent.replaceAll(`=${pharse}`, abbreviations[pharse]);
+        }
     }
 
-    initialize();
-})();
+    return { content: originalContent, replacement: replacement };
+}
